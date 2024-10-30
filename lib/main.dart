db79758d7b503041cb6dd739ba2f8b3b0e2cea5d
anismkb <anismekbal2001@gmail.com>
2024-10-30 08:59:17 +0100

    Hid the .env file

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,26 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+// DotEnv dotenv = DotEnv() is automatically called during import.
+// If you want to load multiple dotenv files or name your dotenv object differently, you can do the following and import the singleton into the relavant files:
+// DotEnv another_dotenv = DotEnv()
+/*
+Future main() async {
+  // To load the .env file contents into dotenv.
+  // NOTE: fileName defaults to .env and can be omitted in this case.
+  // Ensure that the filename corresponds to the path in step 1 and 2.
+  await dotenv.load(fileName: ".env");
+  //...runapp
+}
+*/
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  Stripe.publishableKey=publishableKey;
+  await dotenv.load(fileName: ".env");
+  //Stripe.publishableKey=publishableKey;
+  Stripe.publishableKey= dotenv.env['STRIPE_PUBLISHABLE_KEY']!;
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(const MyApp());
 }
